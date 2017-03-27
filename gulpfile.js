f110ae53702abb4828ca75ea1cedaf8bb6d21c4b
Abhishek Deo <Abhishek_Deo@msn.com>
2017-03-27 07:51:53 -0400

    added ts build, fixed less task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var less= require('gulp-less');
+var tsb = require('gulp-tsb');
 var path = require('path');
+var tsconfig = tsb.create('tsconfig.json');
 
-// compile less files from the ./styles folder
+// compile less files from the ./src/styles folder
 // into css files to the ./public/stylesheets folder
 gulp.task('less', function () {
     return gulp.src('./src/styles/**/*.less')
         .pipe(less({
             paths: [path.join(__dirname, 'less', 'includes')]
         }))
-        .pipe(gulp.dest('./out/public/stylesheets'));
+        .pipe(gulp.dest('./public/stylesheets'));
 });
 
-gulp.task('default', ['less']);
+// typescript builder
+gulp.task('ts', function () {
+    return gulp.src('./src/scripts/**/*.ts')
+        .pipe(tsconfig())
+        .pipe(gulp.dest('./public/util'));
+});
+
+// gulp.task('buildAll', ['build', 'less']);
+gulp.task('default', ['ts', 'less']);
